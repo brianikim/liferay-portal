e5736ae6b7b097c8d990cd9c9e68097dc0c99d50
Igor Beslic <igor.beslic@liferay.com>
2021-04-06 22:12:04 +0200

    COMMERCE-6410 1. headless-commerce-admin-order-impl - prevent failures if shippingAddress regionISOCode is null 2. sort

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/util/v1_0/ShippingAddressUtil.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/util/v1_0/ShippingAddressUtil.java
@@ -94,22 +94,22 @@ public class ShippingAddressUtil {
 			ShippingAddress shippingAddress)
 		throws Exception {
 
-		if (Validator.isNull(shippingAddress.getRegionISOCode()) &&
-			(commerceAddress != null)) {
+		if ((commerceCountry != null) &&
+			Validator.isNotNull(shippingAddress.getRegionISOCode())) {
 
-			return commerceAddress.getCommerceRegionId();
+			CommerceRegion commerceRegion =
+				CommerceRegionLocalServiceUtil.getCommerceRegion(
+					commerceCountry.getCommerceCountryId(),
+					shippingAddress.getRegionISOCode());
+
+			return commerceRegion.getCommerceRegionId();
 		}
 
-		if (commerceCountry == null) {
-			return 0;
+		if (commerceAddress != null) {
+			return commerceAddress.getRegionId();
 		}
 
-		CommerceRegion commerceRegion =
-			CommerceRegionLocalServiceUtil.getCommerceRegion(
-				commerceCountry.getCommerceCountryId(),
-				shippingAddress.getRegionISOCode());
-
-		return commerceRegion.getCommerceRegionId();
+		return 0;
 	}
 
 	private static String _getDescription(CommerceAddress commerceAddress) {
