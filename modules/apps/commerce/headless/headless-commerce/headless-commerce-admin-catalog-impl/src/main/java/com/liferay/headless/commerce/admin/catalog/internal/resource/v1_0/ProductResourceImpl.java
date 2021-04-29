5c7b6b507eea37986822f351afe7846e8c66010e
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2021-04-29 22:52:02 +0200

    COMMERCE-6569 headless-commerce-admincatalog-impl add logic to manage categories when creating/update a product

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
@@ -74,6 +74,7 @@ import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -88,6 +89,7 @@ import com.liferay.upload.UniqueFileNameProvider;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -356,7 +358,23 @@ public class ProductResourceImpl
 					contextCompany.getCompanyId(),
 					product.getExternalReferenceCode());
 
-		if ((product.getCategories() == null) && (cpDefinition != null)) {
+		// Categories
+
+		Category[] categories = product.getCategories();
+
+		if (categories != null) {
+			List<Long> categoryIds = new ArrayList<>();
+
+			for (Category category : categories) {
+				if (category.getId() != null) {
+					categoryIds.add(category.getId());
+				}
+			}
+
+			serviceContext.setAssetCategoryIds(
+				ArrayUtil.toLongArray(categoryIds));
+		}
+		else if (cpDefinition != null) {
 			long[] categoryIds = _assetCategoryLocalService.getCategoryIds(
 				cpDefinition.getModelClassName(),
 				cpDefinition.getCPDefinitionId());
@@ -697,16 +715,6 @@ public class ProductResourceImpl
 			}
 		}
 
-		// Categories
-
-		Category[] categories = product.getCategories();
-
-		if (categories != null) {
-
-			// TODO upsert categories
-
-		}
-
 		// Images
 
 		Attachment[] images = product.getImages();
@@ -792,12 +800,28 @@ public class ProductResourceImpl
 
 		DateConfig expirationDateConfig = new DateConfig(expirationCalendar);
 
-		if (product.getCategories() == null) {
+		// Categories
+
+		Category[] categories = product.getCategories();
+
+		if (categories == null) {
 			long[] categoryIds = _assetCategoryLocalService.getCategoryIds(
 				cpDefinition.getModelClassName(),
 				cpDefinition.getCPDefinitionId());
 
 			serviceContext.setAssetCategoryIds(categoryIds);
+		}
+		else {
+			List<Long> categoryIds = new ArrayList<>();
+
+			for (Category category : categories) {
+				if (category.getId() != null) {
+					categoryIds.add(category.getId());
+				}
+			}
+
+			serviceContext.setAssetCategoryIds(
+				ArrayUtil.toLongArray(categoryIds));
 		}
 
 		Map<String, String> shortDescriptionMap = product.getShortDescription();
