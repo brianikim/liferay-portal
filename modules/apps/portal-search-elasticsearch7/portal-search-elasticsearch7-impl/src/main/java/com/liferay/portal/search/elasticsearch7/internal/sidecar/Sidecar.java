42d8ca291882db4e960635a8a9191326df1a24c8
Bryan Engler <bryan.engler@liferay.com>
2021-09-13 10:01:45 -0700

    LPS-139071 portal-search-elasticsearch7-impl: Update to elastic 7.14.1
    https://issues.liferay.com/browse/LPS-139071

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
@@ -27,6 +27,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.cluster.ClusterExecutor;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -425,8 +426,10 @@ public class Sidecar {
 			return new Elasticsearch790Distribution();
 		}
 
-		if (versionNumber.equals("7.14.0")) {
-			return new Elasticsearch7140Distribution();
+		if (ArrayUtil.contains(
+				_SUPPORTED_ELASTICSEARCH_SIDECAR_VERSIONS, versionNumber)) {
+
+			return new ElasticsearchDistribution(versionNumber);
 		}
 
 		throw new IllegalArgumentException(
@@ -602,6 +605,10 @@ public class Sidecar {
 		).build(
 		).install();
 	}
+
+	private static final String[] _SUPPORTED_ELASTICSEARCH_SIDECAR_VERSIONS = {
+		"7.14.0", "7.14.1"
+	};
 
 	private static final Log _log = LogFactoryUtil.getLog(Sidecar.class);
 
