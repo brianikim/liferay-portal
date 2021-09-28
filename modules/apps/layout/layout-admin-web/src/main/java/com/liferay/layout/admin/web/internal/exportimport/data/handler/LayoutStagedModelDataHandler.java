7cf34033f6f330f661b8923a13f73a2d9192d6dd
Jürgen Kappler <jurgen.kappler@liferay.com>
2021-09-28 13:47:28 +0200

    LPS-139864 Update correct layout

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/exportimport/data/handler/LayoutStagedModelDataHandler.java
@@ -2798,7 +2798,11 @@ public class LayoutStagedModelDataHandler
 			typeSettingsUnicodeProperties.setProperty(
 				"collectionPK", String.valueOf(assetListEntryId));
 
-			layout.setTypeSettingsProperties(typeSettingsUnicodeProperties);
+			importedLayout = _layoutLocalService.getLayout(
+				importedLayout.getPlid());
+
+			importedLayout.setTypeSettingsProperties(
+				typeSettingsUnicodeProperties);
 
 			importedLayout = _layoutLocalService.updateLayout(importedLayout);
 		}
