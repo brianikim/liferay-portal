6982255b7064365bc18dc039097613c1a7cb998d
Danny Situ <danny.situ@liferay.com>
2021-10-05 11:34:17 -0700

    COMMERCE-6410 Adjust for branch differences

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/util/v1_0/ShippingAddressUtil.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/util/v1_0/ShippingAddressUtil.java
@@ -106,7 +106,7 @@ public class ShippingAddressUtil {
 		}
 
 		if (commerceAddress != null) {
-			return commerceAddress.getRegionId();
+			return commerceAddress.getCommerceRegionId();
 		}
 
 		return 0;
