fe601f510517868c93b4fff1cda2e4cb58ee4608
David Tello <david.tello@liferay.com>
2021-12-07 13:34:00 +0100

    LPS-143296 Save the expando attributes first to ensure that the user indexation takes the new expando values

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
@@ -1222,8 +1222,6 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 					ldapImportContext, ldapUser, user, password,
 					modifyTimestamp, isNew);
 
-				updateExpandoAttributes(ldapImportContext, user, ldapUser);
-
 				ldapImportContext.addImportedUserId(
 					fullUserDN, user.getUserId());
 			}
@@ -1573,27 +1571,6 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 		_userLocalService = userLocalService;
 	}
 
-	protected void updateExpandoAttributes(
-			LDAPImportContext ldapImportContext, User user, LDAPUser ldapUser)
-		throws Exception {
-
-		ExpandoBridge userExpandoBridge = user.getExpandoBridge();
-
-		populateExpandoAttributes(
-			userExpandoBridge, ldapUser.getUserExpandoAttributes(),
-			ldapImportContext.getUserExpandoMappings(),
-			ldapImportContext.getLdapUserIgnoreAttributes());
-
-		Contact contact = user.getContact();
-
-		ExpandoBridge contactExpandoBridge = contact.getExpandoBridge();
-
-		populateExpandoAttributes(
-			contactExpandoBridge, ldapUser.getContactExpandoAttributes(),
-			ldapImportContext.getContactExpandoMappings(),
-			ldapImportContext.getLdapUserIgnoreAttributes());
-	}
-
 	protected void updateLDAPUser(
 			User ldapUser, Contact ldapContact, User user,
 			Properties userMappings, Properties contactMappings,
@@ -1757,6 +1734,22 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 		}
 
 		Contact ldapContact = ldapUser.getContact();
+
+		ExpandoBridge userExpandoBridge = user.getExpandoBridge();
+
+		populateExpandoAttributes(
+			userExpandoBridge, ldapUser.getUserExpandoAttributes(),
+			ldapImportContext.getUserExpandoMappings(),
+			ldapImportContext.getLdapUserIgnoreAttributes());
+
+		Contact contact = user.getContact();
+
+		ExpandoBridge contactExpandoBridge = contact.getExpandoBridge();
+
+		populateExpandoAttributes(
+			contactExpandoBridge, ldapUser.getContactExpandoAttributes(),
+			ldapImportContext.getContactExpandoMappings(),
+			ldapImportContext.getLdapUserIgnoreAttributes());
 
 		updateLDAPUser(
 			ldapUser.getUser(), ldapContact, user,
