6707edbcdba1c92d92fae4717690d74d2678afc3
Olivia Yu <olivia.yu@liferay.com>
2021-12-29 16:12:54 -0800

    LPS-143640 search-experiences-web: Use frameworkConfig's clauseContributorsIncludes/Excludes instead of 'enabled' state

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/sxp_blueprint_admin/js/edit_sxp_blueprint/EditSXPBlueprintForm.js
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/sxp_blueprint_admin/js/edit_sxp_blueprint/EditSXPBlueprintForm.js
@@ -370,7 +370,10 @@ function EditSXPBlueprintForm({
 					id: index,
 				})
 			),
-			frameworkConfig: initialConfiguration.generalConfiguration || {},
+			frameworkConfig: initialConfiguration.generalConfiguration || {
+				clauseContributorsExcludes: [],
+				clauseContributorsIncludes: [],
+			},
 			highlightConfig: JSON.stringify(
 				initialConfiguration.highlightConfiguration,
 				null,
