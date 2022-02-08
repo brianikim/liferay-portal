c3b84e709584b642db6c364265276120213383b2
Noor Najjar <noor.najjar@liferay.com>
2022-02-08 15:37:06 -0800

    LPS-146666 Need to pass plid for classPK instead of layoutId

--- portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceHelper.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutLocalServiceHelper.java
@@ -103,14 +103,21 @@ public class LayoutLocalServiceHelper implements IdentifiableOSGiService {
 
 		String originalFriendlyURL = friendlyURL;
 
+		Layout layout = LayoutLocalServiceUtil.fetchLayout(
+			groupId, privateLayout, layoutId);
+
 		for (int i = 1;; i++) {
 			try {
 				validateFriendlyURL(
 					groupId, privateLayout, layoutId, friendlyURL, languageId);
 
 				if (_layoutFriendlyURLEntryValidator != null) {
-					_layoutFriendlyURLEntryValidator.validateFriendlyURLEntry(
-						groupId, privateLayout, layoutId, friendlyURL);
+					if (layout != null) {
+						_layoutFriendlyURLEntryValidator.
+							validateFriendlyURLEntry(
+								groupId, privateLayout, layout.getPlid(),
+								friendlyURL);
+					}
 				}
 
 				break;
@@ -119,9 +126,6 @@ public class LayoutLocalServiceHelper implements IdentifiableOSGiService {
 				int type = layoutFriendlyURLException.getType();
 
 				if (type == LayoutFriendlyURLException.DUPLICATE) {
-					Layout layout = LayoutLocalServiceUtil.fetchLayout(
-						groupId, privateLayout, layoutId);
-
 					if (layout == null) {
 						friendlyURL = originalFriendlyURL + i;
 					}
