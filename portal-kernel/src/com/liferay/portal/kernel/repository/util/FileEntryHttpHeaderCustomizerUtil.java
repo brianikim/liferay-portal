10800420007253a3905522d24983db21e80dd3de
Roberto Díaz <roberto.diaz@liferay.com>
2022-02-11 12:59:22 +0100

    LPS-109977 Fix compilation for 7.3

--- portal-kernel/src/com/liferay/portal/kernel/repository/util/FileEntryHttpHeaderCustomizerUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/repository/util/FileEntryHttpHeaderCustomizerUtil.java
@@ -14,11 +14,18 @@
 
 package com.liferay.portal.kernel.repository.util;
 
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
-import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
-import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.repository.http.header.customizer.FileEntryHttpHeaderCustomizer;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.ServiceTracker;
+import com.liferay.registry.ServiceTrackerCustomizer;
+import com.liferay.registry.util.StringPlus;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * @author Adolfo Pérez
@@ -29,7 +36,8 @@ public class FileEntryHttpHeaderCustomizerUtil {
 		FileEntry fileEntry, String httpHeaderName, String currentValue) {
 
 		FileEntryHttpHeaderCustomizer fileEntryHttpHeaderCustomizer =
-			_serviceTrackerMap.getService(httpHeaderName);
+			_fileEntryHttpHeaderCustomizerUtil.
+				_getFileEntryHttpHeaderCustomizer(httpHeaderName);
 
 		if (fileEntryHttpHeaderCustomizer == null) {
 			return currentValue;
@@ -39,10 +47,79 @@ public class FileEntryHttpHeaderCustomizerUtil {
 			fileEntry, currentValue);
 	}
 
-	private static final ServiceTrackerMap
-		<String, FileEntryHttpHeaderCustomizer> _serviceTrackerMap =
-			ServiceTrackerMapFactory.openSingleValueMap(
-				SystemBundleUtil.getBundleContext(),
-				FileEntryHttpHeaderCustomizer.class, "http.header.name");
+	private FileEntryHttpHeaderCustomizerUtil() {
+		Registry registry = RegistryUtil.getRegistry();
+
+		_serviceTracker = registry.trackServices(
+			FileEntryHttpHeaderCustomizer.class,
+			new FileEntryHttpHeaderCustomizerTrackerCustomizer());
+
+		_serviceTracker.open();
+	}
+
+	private FileEntryHttpHeaderCustomizer _getFileEntryHttpHeaderCustomizer(
+		String httpHeaderName) {
+
+		return _fileEntryHttpHeaderCustomizers.get(httpHeaderName);
+	}
+
+	private static final FileEntryHttpHeaderCustomizerUtil
+		_fileEntryHttpHeaderCustomizerUtil =
+			new FileEntryHttpHeaderCustomizerUtil();
+
+	private final ConcurrentMap<String, FileEntryHttpHeaderCustomizer>
+		_fileEntryHttpHeaderCustomizers = new ConcurrentHashMap<>();
+	private final ServiceTracker
+		<FileEntryHttpHeaderCustomizer, FileEntryHttpHeaderCustomizer>
+			_serviceTracker;
+
+	private class FileEntryHttpHeaderCustomizerTrackerCustomizer
+		implements ServiceTrackerCustomizer
+			<FileEntryHttpHeaderCustomizer, FileEntryHttpHeaderCustomizer> {
+
+		@Override
+		public FileEntryHttpHeaderCustomizer addingService(
+			ServiceReference<FileEntryHttpHeaderCustomizer> serviceReference) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			FileEntryHttpHeaderCustomizer fileEntryHttpHeaderCustomizer =
+				registry.getService(serviceReference);
+
+			List<String> httpHeaderNames = StringPlus.asList(
+				serviceReference.getProperty("http.header.name"));
+
+			for (String httpHeaderName : httpHeaderNames) {
+				_fileEntryHttpHeaderCustomizers.put(
+					httpHeaderName, fileEntryHttpHeaderCustomizer);
+			}
+
+			return fileEntryHttpHeaderCustomizer;
+		}
+
+		@Override
+		public void modifiedService(
+			ServiceReference<FileEntryHttpHeaderCustomizer> serviceReference,
+			FileEntryHttpHeaderCustomizer service) {
+		}
+
+		@Override
+		public void removedService(
+			ServiceReference<FileEntryHttpHeaderCustomizer> serviceReference,
+			FileEntryHttpHeaderCustomizer service) {
+
+			Registry registry = RegistryUtil.getRegistry();
+
+			registry.ungetService(serviceReference);
+
+			List<String> httpHeaderNames = StringPlus.asList(
+				serviceReference.getProperty("http.header.name"));
+
+			for (String httpHeaderName : httpHeaderNames) {
+				_fileEntryHttpHeaderCustomizers.remove(httpHeaderName);
+			}
+		}
+
+	}
 
 }
