d1332b7cde4920d7cf03665a240ffb620a8aaca4
Ivica Cardic <ivica.cardic@liferay.com>
2022-02-18 17:08:19 +0100

    COMMERCE-1490 - Rename

--- modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/display/context/builder/CPOptionsSearchFacetDisplayContextBuilder.java
+++ modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/display/context/builder/CPOptionsSearchFacetDisplayContextBuilder.java
@@ -58,7 +58,7 @@ public class CPOptionsSearchFacetDisplayContextBuilder implements Serializable {
 		CPOptionsSearchFacetDisplayContext cpOptionsSearchFacetDisplayContext =
 			_createCPOptionsSearchFacetDisplayContext();
 
-		cpOptionsSearchFacetDisplayContext.setFacets(getFacets());
+		cpOptionsSearchFacetDisplayContext.setFacets(_getFacets());
 		cpOptionsSearchFacetDisplayContext.setCPOptionLocalService(
 			_cpOptionLocalService);
 		cpOptionsSearchFacetDisplayContext.setPortletSharedSearchResponse(
@@ -69,34 +69,6 @@ public class CPOptionsSearchFacetDisplayContextBuilder implements Serializable {
 			buildTermDisplayContexts());
 
 		return cpOptionsSearchFacetDisplayContext;
-	}
-
-	public List<Facet> getFacets() {
-		List<Facet> filledFacets = new ArrayList<>();
-
-		FacetCollector facetCollector = _facet.getFacetCollector();
-
-		PortletSharedSearchResponse portletSharedSearchResponse =
-			_portletSharedSearchRequest.search(_renderRequest);
-
-		ThemeDisplay themeDisplay = portletSharedSearchResponse.getThemeDisplay(
-			_renderRequest);
-
-		for (TermCollector termCollector : facetCollector.getTermCollectors()) {
-			CPOption cpOption = _cpOptionLocalService.fetchCPOption(
-				themeDisplay.getCompanyId(), termCollector.getTerm());
-
-			if ((cpOption != null) && cpOption.isFacetable()) {
-				Facet facet = portletSharedSearchResponse.getFacet(
-					CPOptionFacetsUtil.getIndexFieldName(
-						termCollector.getTerm(), themeDisplay.getLanguageId()));
-
-				filledFacets.add(facet);
-				_tuples = _getTuples(cpOption, facet.getFacetCollector());
-			}
-		}
-
-		return filledFacets;
 	}
 
 	public void setCPOptionLocalService(
@@ -297,6 +269,34 @@ public class CPOptionsSearchFacetDisplayContextBuilder implements Serializable {
 		).map(
 			cpOption -> buildTermDisplayContext(0, 1, true, StringPool.BLANK)
 		);
+	}
+
+	private List<Facet> _getFacets() {
+		List<Facet> filledFacets = new ArrayList<>();
+
+		FacetCollector facetCollector = _facet.getFacetCollector();
+
+		PortletSharedSearchResponse portletSharedSearchResponse =
+			_portletSharedSearchRequest.search(_renderRequest);
+
+		ThemeDisplay themeDisplay = portletSharedSearchResponse.getThemeDisplay(
+			_renderRequest);
+
+		for (TermCollector termCollector : facetCollector.getTermCollectors()) {
+			CPOption cpOption = _cpOptionLocalService.fetchCPOption(
+				themeDisplay.getCompanyId(), termCollector.getTerm());
+
+			if ((cpOption != null) && cpOption.isFacetable()) {
+				Facet facet = portletSharedSearchResponse.getFacet(
+					CPOptionFacetsUtil.getIndexFieldName(
+						termCollector.getTerm(), themeDisplay.getLanguageId()));
+
+				filledFacets.add(facet);
+				_tuples = _getTuples(cpOption, facet.getFacetCollector());
+			}
+		}
+
+		return filledFacets;
 	}
 
 	private List<Tuple> _getTuples(
