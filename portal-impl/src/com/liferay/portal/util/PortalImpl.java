cef471771edb4218c911f602d18872c50f93aafd
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-03-18 17:31:46 +0100

    LPS-144563 Do not allow unmappable characters

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -934,6 +934,10 @@ public class PortalImpl implements Portal {
 			return url;
 		}
 
+		if (url.contains(_UNICODE_REPLACEMENT_CHARACTER)) {
+			return null;
+		}
+
 		URI uri = HttpUtil.getURI(url);
 
 		if (uri == null) {
@@ -8760,6 +8764,8 @@ public class PortalImpl implements Portal {
 
 	private static final String _PUBLIC_GROUP_SERVLET_MAPPING =
 		PropsValues.LAYOUT_FRIENDLY_URL_PUBLIC_SERVLET_MAPPING;
+
+	private static final String _UNICODE_REPLACEMENT_CHARACTER = "\uFFFD";
 
 	private static final Log _log = LogFactoryUtil.getLog(PortalImpl.class);
 
