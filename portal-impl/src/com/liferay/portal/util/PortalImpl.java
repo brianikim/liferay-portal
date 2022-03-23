4ed0a9c301e8cc2f9c2c49e3f72bf6b33241b4a0
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-03-23 08:52:07 +0100

    LPS-135170 - buy space

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -3904,8 +3904,6 @@ public class PortalImpl implements Portal {
 	public long getPlidFromPortletId(
 		long groupId, boolean privateLayout, String portletId) {
 
-		long plid = LayoutConstants.DEFAULT_PLID;
-
 		StringBundler sb = new StringBundler(5);
 
 		sb.append(groupId);
@@ -3919,41 +3917,42 @@ public class PortalImpl implements Portal {
 		Long plidObj = _plidToPortletIdMap.get(key);
 
 		if (plidObj == null) {
-			plid = doGetPlidFromPortletId(groupId, privateLayout, portletId);
+			long plid = doGetPlidFromPortletId(
+				groupId, privateLayout, portletId);
 
 			if (plid != LayoutConstants.DEFAULT_PLID) {
 				_plidToPortletIdMap.put(key, plid);
 			}
+
+			return plid;
 		}
-		else {
-			plid = plidObj.longValue();
 
-			boolean validPlid = false;
+		long plid = plidObj.longValue();
 
-			try {
-				Layout layout = LayoutLocalServiceUtil.getLayout(plid);
+		boolean validPlid = false;
 
-				LayoutTypePortlet layoutTypePortlet =
-					(LayoutTypePortlet)layout.getLayoutType();
+		try {
+			Layout layout = LayoutLocalServiceUtil.getLayout(plid);
 
-				if (layoutTypePortlet.hasDefaultScopePortletId(
-						groupId, portletId)) {
+			LayoutTypePortlet layoutTypePortlet =
+				(LayoutTypePortlet)layout.getLayoutType();
 
-					validPlid = true;
-				}
+			if (layoutTypePortlet.hasDefaultScopePortletId(
+					groupId, portletId)) {
+
+				validPlid = true;
 			}
-			catch (Exception exception) {
-			}
+		}
+		catch (Exception exception) {
+		}
 
-			if (!validPlid) {
-				_plidToPortletIdMap.remove(key);
+		if (!validPlid) {
+			_plidToPortletIdMap.remove(key);
 
-				plid = doGetPlidFromPortletId(
-					groupId, privateLayout, portletId);
+			plid = doGetPlidFromPortletId(groupId, privateLayout, portletId);
 
-				if (plid != LayoutConstants.DEFAULT_PLID) {
-					_plidToPortletIdMap.put(key, plid);
-				}
+			if (plid != LayoutConstants.DEFAULT_PLID) {
+				_plidToPortletIdMap.put(key, plid);
 			}
 		}
 
