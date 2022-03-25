c3119bf6518e0f1e049f3e6836da1b285faba351
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-03-25 13:13:39 +0100

    LPS-144563 Add test

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplEscapeRedirectTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplEscapeRedirectTest.java
@@ -109,6 +109,9 @@ public class PortalImplEscapeRedirectTest {
 			Assert.assertNull(_portalImpl.escapeRedirect(":@liferay.com"));
 			Assert.assertNull(_portalImpl.escapeRedirect("http:/web"));
 			Assert.assertNull(_portalImpl.escapeRedirect("http:web"));
+			Assert.assertNull(
+				_portalImpl.escapeRedirect(
+					"https://google.com\uFFFD@localhost"));
 		}
 		finally {
 			setPropsValuesValue(
