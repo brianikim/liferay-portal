1ec045d27a0b4f7b5355dc93cbbfd6b1ae85cb6e
Victor Galan <victor.galan@liferay.com>
2022-04-07 15:34:21 +0200

    LPS-145232 The old name contained the name of the vocabulary which can be changed, so don't rely on it in the uniqueId

--- modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemFieldSetProviderImpl.java
+++ modules/apps/asset/asset-info-impl/src/main/java/com/liferay/asset/info/internal/item/provider/AssetEntryInfoItemFieldSetProviderImpl.java
@@ -92,7 +92,7 @@ public class AssetEntryInfoItemFieldSetProviderImpl
 					InfoField.builder(
 					).infoFieldType(
 						CategoriesInfoFieldType.INSTANCE
-					).namespace(
+					).uniqueId(
 						AssetVocabulary.class.getSimpleName() +
 							StringPool.UNDERLINE +
 								assetVocabulary.getVocabularyId()
@@ -205,7 +205,7 @@ public class AssetEntryInfoItemFieldSetProviderImpl
 					InfoField.builder(
 					).infoFieldType(
 						CategoriesInfoFieldType.INSTANCE
-					).namespace(
+					).uniqueId(
 						AssetVocabulary.class.getSimpleName() +
 							StringPool.UNDERLINE +
 								assetVocabulary.getVocabularyId()
