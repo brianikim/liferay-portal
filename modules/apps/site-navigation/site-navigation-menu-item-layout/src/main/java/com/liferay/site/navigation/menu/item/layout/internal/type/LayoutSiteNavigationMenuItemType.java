0ffbce44c0aba102cd1d7ce1f3993150bc7a49ed
Jose L. Bango <jose.bango@liferay.com>
2022-04-11 10:43:59 +0200

    LPS-150132 Don't add layout reference when the layout is private and the publication is for public pages (and vice versa)

--- modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
+++ modules/apps/site-navigation/site-navigation-menu-item-layout/src/main/java/com/liferay/site/navigation/menu/item/layout/internal/type/LayoutSiteNavigationMenuItemType.java
@@ -27,6 +27,8 @@ import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutFriendlyURL;
@@ -97,6 +99,24 @@ public class LayoutSiteNavigationMenuItemType
 		Layout layout = _getLayout(portletDataContext, siteNavigationMenuItem);
 
 		if (layout == null) {
+			return false;
+		}
+
+		boolean privateLayout = layout.isPrivateLayout();
+
+		if (privateLayout != portletDataContext.isPrivateLayout()) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					StringBundler.concat(
+						"SiteNavigationMenuItem ",
+						siteNavigationMenuItem.getSiteNavigationMenuItemId(),
+						" won't be exported because it points to a ",
+						privateLayout ? "private" : "public",
+						" layout. It will be exported when a ",
+						privateLayout ? "private" : "public",
+						" process is performed."));
+			}
+
 			return false;
 		}
 
@@ -599,6 +619,9 @@ public class LayoutSiteNavigationMenuItemType
 			GetterUtil.getBoolean(
 				typeSettingsUnicodeProperties.get("setCustomName")));
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutSiteNavigationMenuItemType.class);
 
 	@Reference
 	private ItemSelector _itemSelector;
