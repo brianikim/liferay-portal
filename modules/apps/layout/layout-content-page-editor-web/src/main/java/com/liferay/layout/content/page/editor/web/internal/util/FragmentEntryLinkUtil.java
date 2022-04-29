e0402222e1d856400e401d72a340f24c46666a42
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-04-29 12:31:35 +0200

    LPS-152405 Avoid null pointer exception if the fragmentRenderer doesn't exist

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/FragmentEntryLinkUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/FragmentEntryLinkUtil.java
@@ -150,9 +150,11 @@ public class FragmentEntryLinkUtil {
 				FragmentRenderer fragmentRenderer =
 					fragmentRendererTracker.getFragmentRenderer(rendererKey);
 
-				fragmentEntryKey = fragmentRenderer.getKey();
+				if (fragmentRenderer != null) {
+					fragmentEntryKey = fragmentRenderer.getKey();
 
-				name = fragmentRenderer.getLabel(themeDisplay.getLocale());
+					name = fragmentRenderer.getLabel(themeDisplay.getLocale());
+				}
 
 				if (Validator.isNotNull(portletId)) {
 					name = PortalUtil.getPortletTitle(
