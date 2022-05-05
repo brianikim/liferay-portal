2aec4e6338443ecd7a534f65869be799ae4f1714
Kevin Tan <kevin.tan@liferay.com>
2022-05-04 17:51:33 -0700

    LPS-147423 search-experiences-web: Fix app menu language key
    Previously wasn't retrieving the translated language key

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/application/list/SearchExperiencesPanelCategory.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/application/list/SearchExperiencesPanelCategory.java
@@ -18,9 +18,11 @@ import com.liferay.application.list.BasePanelCategory;
 import com.liferay.application.list.PanelCategory;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.search.experiences.constants.SXPPanelCategoryKeys;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -44,7 +46,10 @@ public class SearchExperiencesPanelCategory extends BasePanelCategory {
 
 	@Override
 	public String getLabel(Locale locale) {
-		return LanguageUtil.get(locale, "search-experiences");
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "search-experiences");
 	}
 
 }
