37bbdc5f362bf251bd29eda1339b497477e220ac
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-05-19 11:02:28 +0100

    LPS-149255 Remove Keywords Report from Content Performance Panel:
    The endpoint TrafficSources should not be called, as we are no longer support Semrush.

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/data/provider/AnalyticsReportsDataProvider.java
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/java/com/liferay/analytics/reports/web/internal/data/provider/AnalyticsReportsDataProvider.java
@@ -28,7 +28,6 @@ import com.liferay.analytics.reports.web.internal.model.ReferringURL;
 import com.liferay.analytics.reports.web.internal.model.TimeRange;
 import com.liferay.analytics.reports.web.internal.model.TimeSpan;
 import com.liferay.analytics.reports.web.internal.model.TrafficChannel;
-import com.liferay.analytics.reports.web.internal.model.TrafficSource;
 import com.liferay.analytics.reports.web.internal.model.util.TrafficChannelUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -297,9 +296,6 @@ public class AnalyticsReportsDataProvider {
 		throws PortalException {
 
 		try {
-			Map<String, TrafficSource> trafficSourceMap = getTrafficSources(
-				companyId, url);
-
 			List<ReferringURL> domainReferringURLs = getDomainReferringURLs(
 				companyId, url);
 
@@ -320,7 +316,7 @@ public class AnalyticsReportsDataProvider {
 			return stream.map(
 				acquisitionChannel -> TrafficChannelUtil.toTrafficChannel(
 					acquisitionChannel, domainReferringURLs, pageReferringURLs,
-					referringSocialMediaList, trafficSourceMap)
+					referringSocialMediaList, Collections.emptyMap())
 			).map(
 				trafficChannel -> new AbstractMap.SimpleEntry<>(
 					trafficChannel.getName(), trafficChannel)
@@ -331,37 +327,6 @@ public class AnalyticsReportsDataProvider {
 		catch (Exception exception) {
 			throw new PortalException(
 				"Unable to get acquisition channels", exception);
-		}
-	}
-
-	public Map<String, TrafficSource> getTrafficSources(
-		long companyId, String url) {
-
-		try {
-			String response = _asahFaroBackendClient.doGet(
-				companyId, "api/seo/1.0/traffic-sources?url=" + url);
-
-			TypeFactory typeFactory = _objectMapper.getTypeFactory();
-
-			List<TrafficSource> trafficSources = _objectMapper.readValue(
-				response,
-				typeFactory.constructCollectionType(
-					List.class, TrafficSource.class));
-
-			Stream<TrafficSource> trafficSourcesStream =
-				trafficSources.stream();
-
-			return trafficSourcesStream.map(
-				trafficSource -> new AbstractMap.SimpleEntry<>(
-					trafficSource.getName(), trafficSource)
-			).collect(
-				Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)
-			);
-		}
-		catch (Exception exception) {
-			_log.error("Unable to get traffic sources", exception);
-
-			return Collections.emptyMap();
 		}
 	}
 
