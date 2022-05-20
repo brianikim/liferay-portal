bfb50430dbf8f3b1021dd3d6702e3fd9cfe64b61
Michael Bowerman <michael.bowerman@liferay.com>
2022-05-20 16:46:15 -0700

    LPS-154233 Include the visbility type when publishing an AssetVocabulary for the first time.
    This sort of partially backports the commit
    db95f354bdf06b3aa020d0b6f8885b6c39499b89, although it's a bit different
    since that one is calling a new API that was added specifically to that
    ticket.

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/exportimport/data/handler/AssetVocabularyStagedModelDataHandler.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/exportimport/data/handler/AssetVocabularyStagedModelDataHandler.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -216,11 +217,14 @@ public class AssetVocabularyStagedModelDataHandler
 
 			serviceContext.setUuid(vocabulary.getUuid());
 
+			Map<Locale, String> vocabularyTitleMap = getVocabularyTitleMap(
+				portletDataContext.getScopeGroupId(), vocabulary, name);
+
 			importedVocabulary = _assetVocabularyLocalService.addVocabulary(
 				userId, portletDataContext.getScopeGroupId(), StringPool.BLANK,
-				getVocabularyTitleMap(
-					portletDataContext.getScopeGroupId(), vocabulary, name),
-				vocabulary.getDescriptionMap(), vocabulary.getSettings(),
+				vocabularyTitleMap.get(LocaleUtil.getSiteDefault()),
+				vocabularyTitleMap, vocabulary.getDescriptionMap(),
+				vocabulary.getSettings(), vocabulary.getVisibilityType(),
 				serviceContext);
 		}
 		else {
