1c70f7f4877866386224a97ae8a6ef7fed541c7a
arthurchan35 <arthurchan35@gmail.com>
2022-05-25 10:20:36 +0800

    LPS-153080 Simulate hashing process for non existing users

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -6048,6 +6048,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		}
 
 		if (user == null) {
+			PwdAuthenticator.pretendToAuthenticate();
+
 			return Authenticator.DNE;
 		}
 
