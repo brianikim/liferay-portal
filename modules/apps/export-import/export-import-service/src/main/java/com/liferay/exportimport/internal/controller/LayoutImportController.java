a94d046acddff5ac2ba0bb3dfde24c02ac9a5d9b
Brian Chan <brian.chan@liferay.com>
2022-08-03 15:27:35 -0300

    LPS-145927 SF

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/controller/LayoutImportController.java
@@ -1224,8 +1224,10 @@ public class LayoutImportController implements ImportController {
 		String[] sourceAvailableLanguageIds = StringUtil.split(
 			headerElement.attributeValue("available-locales"));
 
-		for (String languageId : sourceAvailableLanguageIds) {
-			if (!LanguageUtil.isAvailableLocale(groupId, languageId)) {
+		for (String sourceAvailableLanguageId : sourceAvailableLanguageIds) {
+			if (!LanguageUtil.isAvailableLocale(
+					groupId, sourceAvailableLanguageId)) {
+
 				LocaleException localeException = new LocaleException(
 					LocaleException.TYPE_EXPORT_IMPORT);
 
