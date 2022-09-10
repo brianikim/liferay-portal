e940000682864892abec453b3c1399d4b922ac24
Brian Chan <brian.chan@liferay.com>
2022-09-10 09:05:58 -0300

    LPS-158806 As used

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -875,9 +875,6 @@ public class PortalImplUnitTest {
 			MockedStatic<PortalUtil> portalUtilMockedStatic)
 		throws PortletException {
 
-		HttpServletResponse httpServletResponse =
-			new DummyHttpServletResponse();
-
 		LayoutTypePortletFactoryUtil layoutTypePortletFactoryUtil =
 			new LayoutTypePortletFactoryUtil();
 
@@ -900,6 +897,9 @@ public class PortalImplUnitTest {
 		).when(
 			portletMode
 		).toString();
+
+		HttpServletResponse httpServletResponse =
+			new DummyHttpServletResponse();
 
 		return ActionResponseFactory.create(
 			_createActionRequest(portletMode), httpServletResponse,
