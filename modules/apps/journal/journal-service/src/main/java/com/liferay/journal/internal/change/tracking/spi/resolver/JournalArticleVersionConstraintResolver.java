e801c3c8011bd05cb08444a92d29c9671b8db4f9
Brian Chan <brian.chan@liferay.com>
2022-09-13 21:10:55 -0300

    LPS-161841 Use ListUtil.filter

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/change/tracking/spi/resolver/JournalArticleVersionConstraintResolver.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/change/tracking/spi/resolver/JournalArticleVersionConstraintResolver.java
@@ -22,11 +22,11 @@ import com.liferay.journal.util.comparator.ArticleVersionComparator;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MathUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -83,25 +83,21 @@ public class JournalArticleVersionConstraintResolver
 				return latestProductionArticle.getVersion();
 			});
 
-		List<JournalArticle> articles = _journalArticleLocalService.getArticles(
-			ctArticle.getGroupId(), ctArticle.getArticleId(), QueryUtil.ALL_POS,
-			QueryUtil.ALL_POS, new ArticleVersionComparator());
-
-		List<JournalArticle> ctCollectionArticles = new ArrayList<>();
-
-		for (JournalArticle article : articles) {
-			if (article.getCtCollectionId() == ctArticle.getCtCollectionId()) {
-				ctCollectionArticles.add(article);
-			}
-		}
+		List<JournalArticle> articles = ListUtil.filter(
+			_journalArticleLocalService.getArticles(
+				ctArticle.getGroupId(), ctArticle.getArticleId(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				new ArticleVersionComparator()),
+			article ->
+				article.getCtCollectionId() == ctArticle.getCtCollectionId());
 
 		double currentVersion = MathUtil.format(
-			latestVersion + (0.1 * ctCollectionArticles.size()), 1, 1);
+			latestVersion + (0.1 * articles.size()), 1, 1);
 
 		CTPersistence ctPersistence =
 			_journalArticleLocalService.getCTPersistence();
 
-		for (JournalArticle article : ctCollectionArticles) {
+		for (JournalArticle article : articles) {
 			article.setVersion(currentVersion);
 
 			_journalArticleLocalService.updateJournalArticle(article);
