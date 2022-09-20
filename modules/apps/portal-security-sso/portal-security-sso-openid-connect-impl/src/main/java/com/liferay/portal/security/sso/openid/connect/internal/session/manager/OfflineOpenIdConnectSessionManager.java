56db73f42a3f6b21f77a4adaf99eed9528979f82
arthurchan35 <arthurchan35@gmail.com>
2022-09-20 23:27:44 +0800

    LPS-162500 Return true from other threads would pre-maturely:
    invalidate httpsession

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/session/manager/OfflineOpenIdConnectSessionManager.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/session/manager/OfflineOpenIdConnectSessionManager.java
@@ -113,11 +113,7 @@ public class OfflineOpenIdConnectSessionManager {
 			OpenIdConnectSession.class.getSimpleName(), key, lockOwner);
 
 		if (!lockOwner.equals(lock.getOwner())) {
-			if (elapsedTime <= lifetime) {
-				return false;
-			}
-
-			return true;
+			return false;
 		}
 
 		accessToken = _extendOpenIdConnectSession(openIdConnectSession);
