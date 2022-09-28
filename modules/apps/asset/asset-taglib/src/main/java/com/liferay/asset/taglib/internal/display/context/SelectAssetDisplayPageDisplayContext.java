ae7fd7fdc6ad8ad7aa77ae2093ed7d51755abc3c
Javier Moral <javier.moral@liferay.com>
2022-09-28 09:01:57 +0200

    LPS-163803 Add isNew param to asset display taglib

--- modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/display/context/SelectAssetDisplayPageDisplayContext.java
+++ modules/apps/asset/asset-taglib/src/main/java/com/liferay/asset/taglib/internal/display/context/SelectAssetDisplayPageDisplayContext.java
@@ -96,6 +96,9 @@ public class SelectAssetDisplayPageDisplayContext {
 		_groupId = GetterUtil.getLong(
 			_httpServletRequest.getAttribute(
 				"liferay-asset:select-asset-display-page:groupId"));
+		_isNew = GetterUtil.getBoolean(
+			_httpServletRequest.getAttribute(
+				"liferay-asset:select-asset-display-page:isNew"));
 		_showPortletLayouts = GetterUtil.getBoolean(
 			_httpServletRequest.getAttribute(
 				"liferay-asset:select-asset-display-page:showPortletLayouts"));
@@ -179,7 +182,7 @@ public class SelectAssetDisplayPageDisplayContext {
 			_getAssetDisplayPageEntry();
 
 		if (assetDisplayPageEntry == null) {
-			if (_classPK != 0) {
+			if (!_isNew) {
 				_displayPageType = AssetDisplayPageConstants.TYPE_NONE;
 			}
 			else if (Validator.isNull(getLayoutUuid())) {
@@ -456,6 +459,7 @@ public class SelectAssetDisplayPageDisplayContext {
 	private final String _eventName;
 	private final long _groupId;
 	private final HttpServletRequest _httpServletRequest;
+	private final boolean _isNew;
 	private final LiferayPortletRequest _liferayPortletRequest;
 	private final LiferayPortletResponse _liferayPortletResponse;
 	private final boolean _showPortletLayouts;
--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/article/display_page.jsp
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/article/display_page.jsp
@@ -21,8 +21,11 @@ String layoutUuid = null;
 
 JournalArticle article = journalDisplayContext.getArticle();
 
+boolean isNew = true;
+
 if (article != null) {
 	layoutUuid = article.getLayoutUuid();
+	isNew = article.isNew();
 }
 
 Layout articleLayout = null;
@@ -51,6 +54,7 @@ JournalEditArticleDisplayContext journalEditArticleDisplayContext = new JournalE
 	classPK="<%= (article != null) ? article.getResourcePrimKey() : 0 %>"
 	classTypeId="<%= journalEditArticleDisplayContext.getDDMStructureId() %>"
 	groupId="<%= journalEditArticleDisplayContext.getGroupId() %>"
+	new="<%= isNew %>"
 	showPortletLayouts="<%= true %>"
 	showViewInContextLink="<%= journalEditArticleDisplayContext.getClassNameId() == JournalArticleConstants.CLASS_NAME_ID_DEFAULT %>"
 />
