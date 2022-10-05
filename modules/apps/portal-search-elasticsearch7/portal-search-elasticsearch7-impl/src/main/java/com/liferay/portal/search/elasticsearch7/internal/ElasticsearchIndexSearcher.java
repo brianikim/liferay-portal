2172f10bbc42d20c0f7b07c8302a5278536a60b9
Joshua Cords <joshua.cords@liferay.com>
2022-10-05 14:41:22 -0700

    LPS-164442 portal-search-elasticsearch7-impl: move track_total_hits to prepare statement
    https://issues.liferay.com/browse/LPS-164442

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
@@ -250,9 +250,6 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			countSearchRequest, getSearchRequest(searchContext), query,
 			searchContext);
 
-		countSearchRequest.setTrackTotalHits(
-			_elasticsearchConfigurationWrapper.trackTotalHits());
-
 		return countSearchRequest;
 	}
 
@@ -320,8 +317,6 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 		searchSearchRequest.setSorts(searchContext.getSorts());
 		searchSearchRequest.setSorts(searchRequest.getSorts());
 		searchSearchRequest.setStats(searchContext.getStats());
-		searchSearchRequest.setTrackTotalHits(
-			_elasticsearchConfigurationWrapper.trackTotalHits());
 
 		return searchSearchRequest;
 	}
@@ -420,6 +415,8 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			searchRequest.getPostFilterComplexQueryParts());
 		baseSearchRequest.setRescores(searchRequest.getRescores());
 		baseSearchRequest.setStatsRequests(searchRequest.getStatsRequests());
+		baseSearchRequest.setTrackTotalHits(
+			_elasticsearchConfigurationWrapper.trackTotalHits());
 
 		setAggregations(baseSearchRequest, searchRequest);
 		setConnectionId(baseSearchRequest, searchRequest);
