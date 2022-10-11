0ea2534d2a7fe60d048953f07c36a5b22ace4faa
Brian Chan <brian.chan@liferay.com>
2022-10-11 20:41:56 -0300

    LPS-164384 SF

--- modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/display/context/AssetDisplayPagesItemSelectorViewDisplayContext.java
+++ modules/apps/asset/asset-display-page-item-selector-web/src/main/java/com/liferay/asset/display/page/item/selector/web/internal/display/context/AssetDisplayPagesItemSelectorViewDisplayContext.java
@@ -159,7 +159,7 @@ public class AssetDisplayPagesItemSelectorViewDisplayContext {
 	}
 
 	private long _getGroupId() {
-		if (Validator.isNotNull(_groupId)) {
+		if (_groupId != null) {
 			return _groupId;
 		}
 
