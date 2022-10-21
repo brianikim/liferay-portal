c739ec9254681494fcef0dbe3b30c9f4fe5a8c67
João Victor Cordeiro <joao.cordeiro@liferay.com>
2022-10-21 17:53:55 -0300

    COMMERCE-9815 - make the "default option" switch also able to be turned off

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionValueRelLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPDefinitionOptionValueRelLocalServiceImpl.java
@@ -619,11 +619,9 @@ public class CPDefinitionOptionValueRelLocalServiceImpl
 			cpDefinitionOptionValueRelPersistence.update(
 				cpDefinitionOptionValueRel);
 
-		if (preselected) {
-			cpDefinitionOptionValueRel =
-				_updateCPDefinitionOptionValueRelPreselected(
-					cpDefinitionOptionValueRel, preselected);
-		}
+		cpDefinitionOptionValueRel =
+			_updateCPDefinitionOptionValueRelPreselected(
+				cpDefinitionOptionValueRel, preselected);
 
 		// Commerce product definition
 
