083aa41d1d43425e7d8b51e83de09862bd9e6dbc
balazssk <balazs.safrany-kovalik@liferay.com>
2022-10-27 13:29:30 +0200

    LPS-164384 7.3 only has SegmentsExperienceConstants.ID_DEFAULT

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/GetPagePreviewMVCResourceCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/GetPagePreviewMVCResourceCommand.java
@@ -46,7 +46,6 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.segments.constants.SegmentsExperienceConstants;
 import com.liferay.segments.constants.SegmentsWebKeys;
-import com.liferay.segments.service.SegmentsExperienceLocalService;
 import com.liferay.taglib.util.ThemeUtil;
 
 import javax.portlet.ResourceRequest;
@@ -127,8 +126,7 @@ public class GetPagePreviewMVCResourceCommand extends BaseMVCResourceCommand {
 		try {
 			long segmentsExperienceId = ParamUtil.getLong(
 				resourceRequest, "segmentsExperienceId",
-				_segmentsExperienceLocalService.
-					fetchDefaultSegmentsExperienceId(selPlid));
+				SegmentsExperienceConstants.ID_DEFAULT);
 
 			resourceRequest.setAttribute(
 				SegmentsWebKeys.SEGMENTS_EXPERIENCE_IDS,
@@ -248,9 +246,6 @@ public class GetPagePreviewMVCResourceCommand extends BaseMVCResourceCommand {
 
 	@Reference
 	private Portal _portal;
-
-	@Reference
-	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
