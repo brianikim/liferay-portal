474ca881e21d95851c282cede6ee434ce3bf7961
shuyangzhou <shuyang.zhou@liferay.com>
2022-11-13 13:00:25 -0800

    LPS-134898 SF

--- modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
+++ modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
@@ -185,8 +185,6 @@ public class ExpandoValueLocalServiceTest {
 
 		long classPK = CounterLocalServiceUtil.increment();
 
-		// Create initial ExpandoValue for each column
-
 		ExpandoTestUtil.addValues(
 			_expandoTable, classPK,
 			new HashMapBuilder<>().<String, Serializable>put(
@@ -195,9 +193,6 @@ public class ExpandoValueLocalServiceTest {
 				"Test Column2", "column2-one"
 			).build());
 
-		// Remove cache to force  ExpandoValuePeristence find methods be
-		// executed against the database to force flush in between.
-
 		_entityCache.clearCache(ExpandoValueImpl.class);
 		_finderCache.clearCache(ExpandoValueImpl.class);
 
@@ -205,21 +200,6 @@ public class ExpandoValueLocalServiceTest {
 			TransactionConfig.Factory.create(
 				Propagation.REQUIRED, new Class<?>[] {Exception.class}),
 			() -> {
-
-				// Add different value for first column so that ExpandoValue
-				// gets modified and ExpandoRow gets updated. ExpandoRow isn't
-				// persisted on the database because transaction hasn't finish
-				// yet.
-				// Add a new different value to second column so that both
-				// ExpandoValue and ExpandoRow are updated. Since ExpandoRow
-				// was updated in the previous addValue we must ensure that
-				// ExpandoRow isn't stale before updating it again. Right now
-				// there's a expandoValuePersistence.fetchByC_R between the
-				// moment that row is retrieved and the moment it gets updated.
-				// That fetch can cause a flush if the value isn't cached so we
-				// must ensure that row has the latest mvcc version before it
-				// gets persisted.
-
 				ExpandoTestUtil.addValues(
 					_expandoTable, classPK,
 					new HashMapBuilder<>().<String, Serializable>put(
@@ -242,8 +222,6 @@ public class ExpandoValueLocalServiceTest {
 
 		long classPK = CounterLocalServiceUtil.increment();
 
-		// Create initial ExpandoValue for each column
-
 		ExpandoTestUtil.addValues(
 			_expandoTable, classPK,
 			new HashMapBuilder<>().<String, Serializable>put(
@@ -252,9 +230,6 @@ public class ExpandoValueLocalServiceTest {
 				"Test Column2", "column2-one"
 			).build());
 
-		// Remove cache to force  ExpandoValuePeristence find methods be
-		// executed against the database to force flush in between.
-
 		_entityCache.clearCache(ExpandoValueImpl.class);
 		_finderCache.clearCache(ExpandoValueImpl.class);
 
@@ -262,23 +237,8 @@ public class ExpandoValueLocalServiceTest {
 			TransactionConfig.Factory.create(
 				Propagation.REQUIRED, new Class<?>[] {Exception.class}),
 			() -> {
-
-				// Add different value so that ExpandoValue gets modified and
-				// ExpandoRow gets updated. ExpandoRow isn't persisted on the
-				// database yet because transaction hasn't finish.
-
 				ExpandoTestUtil.addValue(
 					_expandoTable, column1, classPK, "column1-two");
-
-				// Add a new different value so that both ExpandoValue and
-				// ExpandoRow are updated. Since ExpandoRow was updated in
-				// the previous addValue we must ensure that ExpandoRow
-				// isn't stale before updating it again. Right now there's
-				// a expandoValuePersistence.fetchByC_R between the moment
-				// that row is retrieved and the moment it gets updated. That
-				// fetch can cause a flush if the value isn't cached so we
-				// must ensure that row has the latest mvcc version before it
-				// gets persisted.
 
 				ExpandoTestUtil.addValue(
 					_expandoTable, column2, classPK, "column2-two");
