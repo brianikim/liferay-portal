cc88ad6efd1c986a09519a3c0394d079edd4256d
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-11-17 13:10:51 +0000

    LPS-149255 Fix test.

--- modules/dxp/apps/analytics/analytics-reports-test/src/testIntegration/java/com/liferay/analytics/reports/web/internal/portlet/action/test/GetTrafficSourcesMVCResourceCommandTest.java
+++ modules/dxp/apps/analytics/analytics-reports-test/src/testIntegration/java/com/liferay/analytics/reports/web/internal/portlet/action/test/GetTrafficSourcesMVCResourceCommandTest.java
@@ -176,8 +176,8 @@ public class GetTrafficSourcesMVCResourceCommandTest {
 
 					Assert.assertEquals("organic", jsonObject2.get("name"));
 					Assert.assertEquals(
-						89.20D, Double.valueOf(jsonObject2.getString("share")),
-						0.0);
+						String.format("%.1f", 89.20D),
+						jsonObject2.getString("share"));
 					Assert.assertEquals(3192, jsonObject2.get("value"));
 
 					JSONObject jsonObject3 = jsonArray.getJSONObject(1);
