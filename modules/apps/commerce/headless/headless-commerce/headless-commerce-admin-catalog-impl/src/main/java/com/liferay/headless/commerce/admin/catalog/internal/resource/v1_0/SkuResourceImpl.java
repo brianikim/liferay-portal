8dfd462b9d3f8ee810b7af326653b05193057811
João Victor Cordeiro <joao.cordeiro@liferay.com>
2023-01-12 15:06:19 -0300

    COMMERCE-10604 Make ERC updatable through SKU Patch

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/SkuResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/SkuResourceImpl.java
@@ -344,7 +344,7 @@ public class SkuResourceImpl
 		DateConfig expirationDateConfig = new DateConfig(expirationCalendar);
 
 		cpInstance = _cpInstanceService.updateCPInstance(
-			cpInstance.getExternalReferenceCode(), cpInstance.getCPInstanceId(),
+			sku.getExternalReferenceCode(), cpInstance.getCPInstanceId(),
 			sku.getSku(), sku.getGtin(), sku.getManufacturerPartNumber(),
 			GetterUtil.get(sku.getPurchasable(), cpInstance.isPurchasable()),
 			GetterUtil.get(sku.getWidth(), cpInstance.getWidth()),
