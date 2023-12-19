f06aa3bab1eef548c47afd728ab17c6352f4d5cf
Brian I. Kim <il.kim@liferay.com>
2023-12-19 14:24:30 -0800

    COMMERCE-13074 Implement Document as nested field for Attachment

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/catalog/resource/v1_0/test/DocumentResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/catalog/resource/v1_0/test/DocumentResourceTest.java
@@ -6,21 +6,36 @@
 package com.liferay.headless.commerce.delivery.catalog.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.product.constants.CPAttachmentFileEntryConstants;
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryLocalService;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.commerce.test.util.CommerceTestUtil;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.headless.commerce.delivery.catalog.client.dto.v1_0.Document;
+import com.liferay.headless.commerce.delivery.catalog.client.serdes.v1_0.DocumentSerDes;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.test.rule.Inject;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -47,20 +62,58 @@ public class DocumentResourceTest extends BaseDocumentResourceTestCase {
 			testGroup.getGroupId(), DLFolderConstants.DEFAULT_PARENT_FOLDER_ID,
 			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
 			null, RandomTestUtil.nextDate(), _serviceContext);
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			testGroup.getGroupId(), RandomTestUtil.randomString());
+		_cpDefinition = CPTestUtil.addCPDefinition(
+			testGroup.getGroupId(), "simple", true, false);
 	}
 
-	@Ignore
 	@Override
 	@Test
 	public void testGetAttachmentIdDocument() throws Exception {
-		super.testGetAttachmentIdDocument();
+		Document postDocument = testGetAttachmentIdDocument_addDocument();
+
+		CPAttachmentFileEntry cpAttachmentFileEntry =
+			_addCPAttachmentFileEntry();
+
+		Document getDocument = documentResource.getAttachmentIdDocument(
+			cpAttachmentFileEntry.getCPAttachmentFileEntryId());
+
+		Assert.assertTrue(equals(postDocument, getDocument));
+		assertValid(getDocument);
 	}
 
-	@Ignore
-	@Override
 	@Test
 	public void testGraphQLGetAttachmentIdDocument() throws Exception {
-		super.testGraphQLGetAttachmentIdDocument();
+		Document document = testGraphQLGetAttachmentIdDocument_addDocument();
+
+		Assert.assertTrue(
+			equals(
+				document,
+				DocumentSerDes.toDTO(
+					JSONUtil.getValueAsString(
+						invokeGraphQLQuery(
+							new GraphQLField(
+								"attachmentIdDocument",
+								HashMapBuilder.<String, Object>put(
+									"id",
+									() -> {
+										CPAttachmentFileEntry
+											cpAttachmentFileEntry =
+												_addCPAttachmentFileEntry();
+
+										return cpAttachmentFileEntry.
+											getCPAttachmentFileEntryId();
+									}
+								).build(),
+								getGraphQLFields())),
+						"JSONObject/data", "Object/attachmentIdDocument"))));
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"externalReferenceCode"};
 	}
 
 	@Override
@@ -73,6 +126,37 @@ public class DocumentResourceTest extends BaseDocumentResourceTestCase {
 	@Override
 	protected Document testGraphQLDocument_addDocument() throws Exception {
 		return _toDocument();
+	}
+
+	private CPAttachmentFileEntry _addCPAttachmentFileEntry() throws Exception {
+		Calendar displayDate = Calendar.getInstance();
+		Calendar expirationDate = Calendar.getInstance();
+
+		displayDate.setTime(RandomTestUtil.nextDate());
+		expirationDate.setTime(RandomTestUtil.nextDate());
+
+		CPAttachmentFileEntry attachmentFileEntry =
+			_cpAttachmentFileEntryLocalService.addCPAttachmentFileEntry(
+				RandomTestUtil.randomString(), _user.getUserId(),
+				testGroup.getGroupId(),
+				_classNameLocalService.getClassNameId(CPDefinition.class),
+				_cpDefinition.getCPDefinitionId(), _fileEntry.getFileEntryId(),
+				false, null, displayDate.get(Calendar.MONTH),
+				displayDate.get(Calendar.DAY_OF_MONTH),
+				displayDate.get(Calendar.YEAR), displayDate.get(Calendar.HOUR),
+				displayDate.get(Calendar.MINUTE),
+				expirationDate.get(Calendar.MONTH),
+				expirationDate.get(Calendar.DAY_OF_MONTH),
+				expirationDate.get(Calendar.YEAR),
+				expirationDate.get(Calendar.HOUR),
+				expirationDate.get(Calendar.MINUTE), true, true,
+				RandomTestUtil.randomLocaleStringMap(), null,
+				RandomTestUtil.nextDouble(),
+				CPAttachmentFileEntryConstants.TYPE_OTHER, _serviceContext);
+
+		_attachmentFileEntries.add(attachmentFileEntry);
+
+		return attachmentFileEntry;
 	}
 
 	private Document _toDocument() throws Exception {
@@ -89,12 +173,27 @@ public class DocumentResourceTest extends BaseDocumentResourceTestCase {
 		};
 	}
 
+	@DeleteAfterTestRun
+	private final List<CPAttachmentFileEntry> _attachmentFileEntries =
+		new ArrayList<>();
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@Inject
+	private CPAttachmentFileEntryLocalService
+		_cpAttachmentFileEntryLocalService;
+
+	@DeleteAfterTestRun
+	private CPDefinition _cpDefinition;
+
 	@Inject
 	private DLAppLocalService _dlAppLocalService;
 
-	@DeleteAfterTestRun
 	private FileEntry _fileEntry;
-
 	private ServiceContext _serviceContext;
 
 	@DeleteAfterTestRun
