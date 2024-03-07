495e64079164e4bdd5906daa0d58fe3df2a1f6eb
Brian I. Kim <il.kim@liferay.com>
2024-03-07 05:26:08 -0800

    LPD-18470 Functional Test

--- modules/test/playwright/pages/users-admin-web/UsersAndOrganizationsPage.ts
+++ modules/test/playwright/pages/users-admin-web/UsersAndOrganizationsPage.ts
@@ -7,6 +7,7 @@
 
 import {Locator, Page} from '@playwright/test';
 
+import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
 import {ApplicationsMenuPage} from '../product-navigation-applications-menu/ApplicationsMenuPage';
 
 export const searchTableRowByValue = async function (
@@ -38,12 +39,26 @@ export const searchTableRowByValue = async function (
 
 export class UsersAndOrganizationsPage {
 	readonly applicationsMenuPage: ApplicationsMenuPage;
+	readonly contactLink: Locator;
+	readonly editOrgLaborIconMenu: Locator;
 	readonly optionsMenu: Locator;
 	readonly page: Page;
 	readonly pageTitle: Locator;
 	readonly exportUsersOptionsMenuItem: Locator;
 	readonly manageCustomFieldsOptionsMenuItem: Locator;
+	readonly openingHoursLink: Locator;
+	readonly organizationActionsMenu: (
+		organizationName: string
+	) => Promise<Locator>;
+	readonly organizationEditMenuItem: Locator;
 	readonly organizationsLink: Locator;
+	readonly organizationsTable: Locator;
+	readonly organizationsTableRow: (
+		colPosition: number,
+		value: string,
+		strictEqual?: boolean
+	) => Promise<{column: Locator; row: Locator}>;
+	readonly orgLaborListTypeSelectedValue: Locator;
 	readonly exportImportOptionsMenuItem: Locator;
 	readonly usersTableRow: (
 		colPosition: number,
@@ -56,6 +71,8 @@ export class UsersAndOrganizationsPage {
 
 	constructor(page: Page) {
 		this.applicationsMenuPage = new ApplicationsMenuPage(page);
+		this.contactLink = page.getByRole('link', {name: 'Contact'});
+		this.editOrgLaborIconMenu = page.getByTestId('editOrgLaborIconMenu');
 		this.exportImportOptionsMenuItem = page.getByRole('menuitem', {
 			name: 'Export / Import',
 		});
@@ -65,12 +82,59 @@ export class UsersAndOrganizationsPage {
 		this.manageCustomFieldsOptionsMenuItem = page.getByRole('menuitem', {
 			name: 'Manage Custom Fields',
 		});
+		this.openingHoursLink = page.getByRole('link', {name: 'Opening Hours'});
 		this.optionsMenu = page
 			.getByTestId('headerOptions')
 			.getByLabel('Options');
+		this.organizationActionsMenu = async (organizationName: string) => {
+			const organizationsTableRow = await this.organizationsTableRow(
+				1,
+				organizationName,
+				true
+			);
+
+			if (organizationsTableRow && organizationsTableRow.row) {
+				const organizationActionsMenu =
+					organizationsTableRow.row.getByLabel('Show Actions');
+
+				if (organizationActionsMenu) {
+					return organizationActionsMenu;
+				}
+			}
+			else {
+				throw new Error(
+					`Cannot locate organization row with organizationName ${organizationName}`
+				);
+			}
+
+			throw new Error(`Cannot locate button with label: Show Actions`);
+		};
+		this.organizationEditMenuItem = page.getByRole('menuitem', {
+			name: 'Edit',
+		});
 		this.organizationsLink = page.getByRole('link', {
 			name: 'Organizations',
 		});
+		this.organizationsTable = page.locator(
+			'#_com_liferay_users_admin_web_portlet_UsersAdminPortlet_organizationsSearchContainer'
+		);
+		this.organizationsTableRow = async (
+			colPosition: number,
+			value: string,
+			strictEqual: boolean = false
+		) => {
+			return await searchTableRowByValue(
+				this.organizationsTable,
+				colPosition,
+				value,
+				strictEqual
+			);
+		};
+		this.orgLaborListTypeSelectedValue = page
+			.locator(
+				'#_com_liferay_users_admin_web_portlet_UsersAdminPortlet_orgLaborListTypeId'
+			)
+			.locator('option[selected=""]');
 		this.page = page;
 		this.pageTitle = page.getByTestId('headerTitle');
 		this.usersTableRow = async (
@@ -106,6 +170,18 @@ export class UsersAndOrganizationsPage {
 
 	async goto() {
 		await this.applicationsMenuPage.goToUsersAndOrganizations();
+	}
+
+	async gotoOrganizationEditOpeningHoursTab(organizationName: string) {
+		await (await this.organizationActionsMenu(organizationName)).click();
+		await this.organizationEditMenuItem.click();
+		await this.contactLink.click();
+		await this.openingHoursLink.click();
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: this.organizationEditMenuItem,
+			trigger: this.editOrgLaborIconMenu,
+		});
 	}
 
 	async goToOrganizations() {
