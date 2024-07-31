a6dcc6c5d3668ae63a580c7619c71613d91732f3
Brian I. Kim <il.kim@liferay.com>
2024-07-30 23:59:40 -0700

    LPD-20381 Create Paypal SDK Integration (Checkout Step Frontend)

---
+++ workspaces/liferay-paypal-workspace/client-extensions/liferay-paypal-commerce-checkout-step/assets/paypal.js
@@ -0,0 +1,102 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export async function onPaypalLoaded() {
+	const payPalOAuth = Liferay.OAuth2Client.FromUserAgentApplication(
+		'liferay-paypal-commerce-payment-integration-oauth-application-user-agent'
+	);
+
+	const orderId = document.getElementById('payment-order-id').value;
+
+	const payPalButton = paypal.Buttons({
+		async createOrder(data) {
+			try {
+				const cartPaymentResource = await payPalOAuth.fetch('/render', {
+					body: JSON.stringify({
+						orderId,
+						fundingSource: data.paymentSource,
+						redirect: false,
+					}),
+					method: 'POST',
+				});
+
+				const cartPaymentURLJson = await cartPaymentResource.json();
+				const cartPaymentURL = cartPaymentURLJson.url;
+				const resource = await fetch(cartPaymentURL);
+
+				if (resource.ok) {
+					const orderData = await payPalOAuth.fetch(
+						'/set-up-payment/get/' + orderId
+					);
+
+					if (orderData) {
+						const orderDataJson = await orderData.json();
+
+						return orderDataJson.id;
+					}
+				}
+			}
+			catch (error) {
+				console.error(error);
+				resultMessage(
+					`Could not initiate PayPal Checkout...<br><br>${error}`
+				);
+			}
+		},
+		async onApprove(data, actions) {
+			try {
+				const cartPaymentResource = await payPalOAuth.fetch('/render', {
+					body: JSON.stringify({
+						orderId,
+						transactionCode: data.orderID,
+					}),
+					method: 'POST',
+				});
+
+				const cartPaymentResourceJSON =
+					await cartPaymentResource.json();
+				const cartPaymentURL = cartPaymentResourceJSON.url;
+				const response = await fetch(cartPaymentURL);
+
+				if (response.ok) {
+					window.location.href = response.url;
+				}
+			}
+			catch (error) {
+				console.error(error);
+				resultMessage(
+					`Sorry, your transaction could not be processed...<br><br>${error}`
+				);
+			}
+		},
+		async onCancel(data) {
+			const cartPaymentResource = await payPalOAuth.fetch('/render', {
+				body: JSON.stringify({
+					cancel: true,
+					orderId,
+					transactionCode: data.orderID,
+					redirect: false,
+				}),
+				method: 'POST',
+			});
+
+			const cartPaymentResourceJSON = await cartPaymentResource.json();
+			const cartPaymentURL = cartPaymentResourceJSON.url;
+			const response = await fetch(cartPaymentURL);
+		},
+		async onError(err) {
+			console.log(err);
+		},
+	});
+
+	if (payPalButton.isEligible()) {
+		payPalButton.render('#paypal-button-container');
+	}
+}
+
+function resultMessage(message) {
+	const container = document.querySelector('#result-message');
+	container.innerHTML = message;
+}
