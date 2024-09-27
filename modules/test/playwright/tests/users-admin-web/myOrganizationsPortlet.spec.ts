80631ec665367d1be47c44344f0be6a27cd10cda
Lianne Louie <lianne.louie@liferay.com>
2024-09-26 18:36:25 -0700

    LPD-37376 - add playwright test

--- modules/test/playwright/tests/users-admin-web/myOrganizationsPortlet.spec.ts
+++ modules/test/playwright/tests/users-admin-web/myOrganizationsPortlet.spec.ts
@@ -9,6 +9,7 @@ import {dataApiHelpersTest} from '../../fixtures/dataApiHelpersTest';
 import {loginTest} from '../../fixtures/loginTest';
 import {usersAndOrganizationsPagesTest} from '../../fixtures/usersAndOrganizationsPagesTest';
 import {UsersAndOrganizationsPage} from '../../pages/users-admin-web/UsersAndOrganizationsPage';
+import getRandomString from '../../utils/getRandomString';
 
 export const test = mergeTests(
 	dataApiHelpersTest,
@@ -85,4 +86,97 @@ test('LPD-35306 Breadcrumb in My Organizations does not have a link if user does
 			organization2.name
 		)
 	).toHaveCount(1);
+});
+
+test('LPD-35093 organization in my organizations portlet should be visible with update suborganizations permission', async ({
+	apiHelpers,
+	context,
+	page,
+	usersAndOrganizationsPage,
+}) => {
+	const organization = await apiHelpers.headlessAdminUser.postOrganization();
+	const organization2 = await apiHelpers.headlessAdminUser.postOrganization({
+		parentOrganization: {
+			externalReferenceCode: organization.externalReferenceCode,
+		},
+	});
+
+	const user = await apiHelpers.headlessAdminUser.postUserAccount();
+
+	await apiHelpers.headlessAdminUser.assignUserToOrganizationByEmailAddress(
+		organization.id,
+		user.emailAddress
+	);
+
+	const companyId = await page.evaluate(() => {
+		return Liferay.ThemeDisplay.getCompanyId();
+	});
+
+	const role = await apiHelpers.headlessAdminUser.postRole({
+		name: getRandomString(),
+		rolePermissions: [
+			{
+				actionIds: [
+					'MANAGE_ORGANIZATIONS',
+					'MANAGE_USERS',
+					'UPDATE',
+					'VIEW',
+					'VIEW_ORGANIZATIONS',
+				],
+				primaryKey: companyId,
+				resourceName: 'com.liferay.account.model.AccountEntry',
+				scope: 1,
+			},
+			{
+				actionIds: [
+					'MANAGE_AVAILABLE_ACCOUNTS',
+					'UPDATE_SUBORGANIZATIONS',
+				],
+				primaryKey: companyId,
+				resourceName: 'com.liferay.portal.kernel.model.Organization',
+				scope: 1,
+			},
+			{
+				actionIds: ['ACCESS_IN_CONTROL_PANEL'],
+				primaryKey: companyId,
+				resourceName:
+					'com_liferay_account_admin_web_internal_portlet_AccountEntriesAdminPortlet',
+				scope: 1,
+			},
+		],
+		roleType: 'organization',
+	});
+
+	await apiHelpers.headlessAdminUser.assignUserToOrganizationRole(
+		role.id,
+		user.id,
+		organization.id
+	);
+
+	await usersAndOrganizationsPage.goToUsers();
+
+	await (
+		await usersAndOrganizationsPage.usersTableRowActions(
+			`${user.alternateName}`
+		)
+	).click();
+
+	const pagePromise = context.waitForEvent('page');
+
+	await usersAndOrganizationsPage.impersonateUserMenuItem.click();
+
+	const newPage = await pagePromise;
+	const newPageUsersAndOrganizationsPage = new UsersAndOrganizationsPage(
+		newPage
+	);
+
+	await newPageUsersAndOrganizationsPage.goToMyOrganizations();
+
+	await expect(
+		usersAndOrganizationsPage.myOrganizationsTableRowLink(organization)
+	).toBeVisible();
+
+	await expect(
+		usersAndOrganizationsPage.myOrganizationsTableRowLink(organization2)
+	).toBeVisible();
 });
