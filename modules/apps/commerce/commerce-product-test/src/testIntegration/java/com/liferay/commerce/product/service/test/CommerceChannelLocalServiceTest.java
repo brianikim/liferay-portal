ded2899540b78df75caeb97232d4f029f78fd6b2
Brian I. Kim <il.kim@liferay.com>
2026-09-24 21:33:03 -0700

    LPD-106244-Grouped-8 Migrate channel search bar test to integration

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CommerceChannelLocalServiceTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CommerceChannelLocalServiceTest.java
@@ -15,6 +15,7 @@ import com.liferay.commerce.product.exception.NoSuchChannelException;
 import com.liferay.commerce.product.model.CommerceChannel;
 import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.lazy.referencing.LazyReferencingThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
@@ -22,6 +23,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.AssertUtils;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
@@ -31,6 +33,10 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -143,6 +149,39 @@ public class CommerceChannelLocalServiceTest {
 	}
 
 	@Test
+	public void testSearch() throws Exception {
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.addCommerceChannel(
+				null, AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT,
+				_group.getGroupId(), RandomTestUtil.randomString(),
+				CommerceChannelConstants.CHANNEL_TYPE_SITE, null,
+				_commerceCurrency.getCode(), _serviceContext);
+
+		_commerceChannels.add(commerceChannel);
+
+		_commerceChannels.add(
+			_commerceChannelLocalService.addCommerceChannel(
+				null, AccountConstants.ACCOUNT_ENTRY_ID_DEFAULT,
+				_group.getGroupId(), RandomTestUtil.randomString(),
+				CommerceChannelConstants.CHANNEL_TYPE_SITE, null,
+				_commerceCurrency.getCode(), _serviceContext));
+
+		Assert.assertEquals(
+			Collections.singletonList(commerceChannel),
+			_commerceChannelLocalService.search(
+				_serviceContext.getCompanyId(), commerceChannel.getName(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null));
+
+		List<CommerceChannel> commerceChannels =
+			_commerceChannelLocalService.search(
+				_serviceContext.getCompanyId(), RandomTestUtil.randomString(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Assert.assertTrue(
+			commerceChannels.toString(), commerceChannels.isEmpty());
+	}
+
+	@Test
 	public void testUpdateCommerceChannel() throws Exception {
 		CommerceChannel commerceChannel =
 			_commerceChannelLocalService.addCommerceChannel(
@@ -168,6 +207,9 @@ public class CommerceChannelLocalServiceTest {
 
 	@Inject
 	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@DeleteAfterTestRun
+	private final List<CommerceChannel> _commerceChannels = new ArrayList<>();
 
 	private CommerceCurrency _commerceCurrency;
 	private Group _group;
